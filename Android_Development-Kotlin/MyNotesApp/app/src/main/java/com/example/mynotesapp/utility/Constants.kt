8f24da7361daf407553554ad92752dc47310fb87
Valeri Dimov <89461040+ValeriDimov@users.noreply.github.com>
2023-12-30 16:12:49 +0200

    Delete Android_Development-Kotlin directory

--- Android_Development-Kotlin/MyNotesApp/app/src/main/java/com/example/mynotesapp/utility/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mynotesapp.utility
-
-object Constants {
-
-    const val KEY_SHARED_PREFS_NAME = "KEY_SHARED_PREFS_NAME"
-
-    const val KEY_NOTE_STATUS_NEW = "KEY_NOTE_STATUS_NEW"
-
-}
--- Android_Development-Kotlin/TaskManagerApp/app/src/main/java/com/example/taskmanagerapp/utility/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.taskmanagerapp.utility
-
-object Constants {
-    const val KEY_REVIEW_TASK_NAME = "KEY_REVIEW_TASK_NAME"
-    const val KEY_REVIEW_TASK_DESC = "KEY_REVIEW_TASK_DESC"
-}
